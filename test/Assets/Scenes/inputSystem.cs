2ec0c57f4bacfeaec585e5ccb5751a3a9a0af9ee
yendo19 <90066698+yendo19@users.noreply.github.com>
2022-04-14 09:03:41 -0400

    Add files via upload

---
+++ test/Assets/Scenes/inputSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputSystem : MonoBehaviour
+{
+    public void testFire()
+    {
+        Debug.Log("Firing");
+    }
+
+    public void testMove()
+    {
+        Debug.Log("Moving");
+    }
+}
